e4d108a1a6652a6474a0c52fe06338ffaa004a77
RaulMartinezEspinosa <33634679+RaulMartinezEspinosa@users.noreply.github.com>
2018-10-17 15:09:51 +0200

    Combate

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour {
+    
+
+    #region Singleton
+
+    public static Level instance;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (instance != null)
+        {
+            Debug.LogError("More than one instance of level is trying to active");
+            return;
+        }
+
+        instance = this;
+    }
+    #endregion
+
+    private int level = 1;
+
+    public int GetLevel()
+    {
+        return level;
+    }
+
+    public void SetLevel()
+    {
+        level++;
+    }
+}
