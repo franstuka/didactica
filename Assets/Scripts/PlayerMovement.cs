cd6d1b8b03d3cf98860f74f81d63335c11faf655
Fran Escriche Grau <franchupinazo@hotmail.es>
2018-11-09 17:37:32 +0100

    solved error

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    Navegation nav;
+    
+
+    private void Start()
+    {
+        nav = GetComponent<Navegation>();
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("HAIL");
+            RaycastHit hit;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, 13)) //layer 13 click detection
+            {
+                Debug.Log("NAIN");
+                nav.SetDestinationPlayer(hit.point);
+            }
+        }
+
 	}
 }
