ddf3860fefab12ca28ba27e18377373d72366dda
Fran Escriche Grau <franchupinazo@hotmail.es>
2018-11-11 11:08:05 +0100

    update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,16 +7,34 @@ public class GameManager : MonoBehaviour {
     #region Singleton
 
     public static GameManager instance;
+    public static SaveDataManager saveDataManager;
 
     private void Awake()
     {
+        bool error = false;
+
         if (instance != null)
         {
             Debug.LogError("More than one instance of GameManager is trying to active");
+            error = true;
+        }
+        if (saveDataManager != null)
+        {
+            Debug.LogError("More than one instance of saveDataManager is trying to active");
+            error = true;
+        }
+        if(error)
+        {
+            Destroy(gameObject);
             return;
         }
-
+        DontDestroyOnLoad(gameObject);
         instance = this;
+        saveDataManager = GetComponent<SaveDataManager>();
+        if(saveDataManager == null)
+        {
+            saveDataManager = gameObject.AddComponent<SaveDataManager>();
+        }
     }
 
     #endregion
