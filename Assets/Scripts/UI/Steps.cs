df4fe71e57fc9026740591c2cbcc67abe38fcc99
RaulMartinezEspinosa <33634679+RaulMartinezEspinosa@users.noreply.github.com>
2018-11-20 05:17:55 +0100

    Arreglo UI pasos

--- Assets/Scripts/UI/Steps.cs
+++ Assets/Scripts/UI/Steps.cs
@@ -5,26 +5,35 @@ using TMPro;
 
 public class Steps : MonoBehaviour {
     private TextMeshProUGUI textMeshPro;
-    private int red;
-    private int blue;
-    private int auxSteps;
-    private float auxPercentage;
+    private float red;
+    private float blue;
+    private int maxSteps;
+    private float percentage;
 
 	// Use this for initialization
 	void Start () {
         textMeshPro = GetComponent<TextMeshProUGUI>();
         red = 0;
         blue = 255;
-        auxSteps = PlayerMovement.movementsAvaible;
-        auxPercentage = 0.0f;
+        maxSteps = PlayerMovement.movementsAvaible;
+        percentage = 0.0f;
     }
 	
 	// Update is called once per frame
-	void Update () {              
-        textMeshPro.text = "" + PlayerMovement.movementsAvaible;        
-        auxPercentage = ((float)PlayerMovement.movementsAvaible / (float)auxSteps) * 100;
-        red = (int)((255 / 100) * (100 - auxPercentage));
-        blue = (int)((255 / 100) * (auxPercentage));
+	void Update () {       
+        if (PlayerMovement.movementsAvaible <= 0)
+        {
+            textMeshPro.text = "0";
+            percentage = 0;
+        }
+        else
+        {
+            textMeshPro.text = "" + PlayerMovement.movementsAvaible;
+            percentage = ((float)PlayerMovement.movementsAvaible / (float)maxSteps) * 100;
+        }
+        
+        red = (255 / 100) * (100 - percentage);
+        blue = (255 / 100) * (percentage);
         textMeshPro.color = new Color32((byte)red, 0, (byte)blue, 255);
     }
 }
