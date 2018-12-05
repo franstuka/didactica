c3c294583041718f79e68a60b06003fb8813ad30
RaulMartinezEspinosa <33634679+RaulMartinezEspinosa@users.noreply.github.com>
2018-12-05 22:22:46 +0100

    Giro al abrir el cofre

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,11 +8,13 @@ public class PlayerMovement : CombatStats
     public int movementsAvaible = 10;
     [SerializeField] Navegation nav;
     private bool canMove;
+    private bool openingChest;
 
     private void Awake()
     {
         nav = GetComponent<Navegation>();
         canMove = true;
+        openingChest = false;
     }
 
     // Update is called once per frame
@@ -22,6 +24,12 @@ public class PlayerMovement : CombatStats
             animations.SetBool("isWalking", false);
             if(!animations.GetBool("openingAChest"))
                 canMove = true;
+        }
+
+        if (openingChest)
+        {
+            animations.SetBool("openingAChest", true);
+            StartCoroutine(endOpeningChestAnimation());
         }
         
         if (canMove) {
@@ -42,12 +50,9 @@ public class PlayerMovement : CombatStats
                     if (hit.collider.tag == "Chest") //layer 11 click detection
                     {                        
                         
-                        if (nav.CanOpenChest(hit.point)) {
-                            //transform.LookAt(hit.transform.position);
-                            //FaceObjective(hit.transform.position);
-                            animations.SetBool("openingAChest", true);
-                            canMove = false;
-                            StartCoroutine(endOpeningChestAnimation());
+                        if (nav.CanOpenChest(hit.point)) {                            
+                            StartCoroutine(spinning(hit.point));                            
+                            canMove = false;                            
                         }
                     }
                     else
@@ -72,11 +77,49 @@ public class PlayerMovement : CombatStats
         canMove = state;
     }
 
+    private bool FaceAndCheckObjective(Vector3 targetPos, float stoppingPrecision)  {
+        Quaternion angle = CaculateTargetRotation(targetPos);
+        if (Mathf.Abs(Quaternion.Angle(angle, transform.rotation)) <= Mathf.Abs(stoppingPrecision))
+        {
+            return true; 
+        }
+        else return false;
+    }
+
+    private void FaceObjective(Vector3 targetPos)
+    {
+        float step = nav.angularSpeed * Time.deltaTime;
+        Quaternion angle = CaculateTargetRotation(targetPos);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, angle, step);
+    }
+
+    private Quaternion CaculateTargetRotation(Vector3 targetPos)
+    {
+
+        float angle = Mathf.Atan2(targetPos.x - transform.position.x, targetPos.z - transform.position.z);
+
+        angle = 180 * angle / Mathf.PI;
+        Vector3 vector3 = new Vector3(0, angle, 0);
+        return Quaternion.Euler(vector3);
+    }
+
+    IEnumerator spinning(Vector3 hit)
+    {
+        for (; !FaceAndCheckObjective(hit, 3f);)
+        {
+            FaceObjective(hit);
+            yield return new WaitForEndOfFrame();
+        }
+        
+        openingChest = true;
+    }
+
     IEnumerator endOpeningChestAnimation()
     {
         yield return new WaitForSeconds(1.2f);
         animations.SetBool("openingAChest", false);
         canMove = true;
+        openingChest = false;
     }
   
 
