7d91247abb7e20998ef374ba6a60e860307434ec
Antonio <al285831@uji.es>
2018-12-20 14:23:10 +0100

    MATAR

---
+++ Assets/SalirNivel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SalirNivel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter(Collision other) {
+		if(other.transform.tag == "Player"){
+			 SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+
+		}
+	}
+}
